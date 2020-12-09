c4d8099b9a3fcd79cf150e01c3c9222d13ebb7e0
domanic-yalda <domanic.yalda@ryerson.ca>
2020-12-09 14:29:22 -0500

    Working Logger- single test

--- src/main/java/com/revature/launcher/BankLauncher.java
+++ src/main/java/com/revature/launcher/BankLauncher.java
@@ -1,13 +1,19 @@
 package com.revature.launcher;
 
+import org.apache.logging.log4j.*;
+
 public class BankLauncher {
 
+	public static Logger bankLogger = LogManager.getLogger("com.revature.bankLogger");
+	
 	public static void main(String[] args) {
 		
-		System.out.println("project-zero");
+		System.out.println("Starting Banking Application");
+		bankLogger.info("server is operation");
 		while(true) {
 			//server running to get using input and server outputs
 			//controller layer
+			break;
 		}
 
 	}
